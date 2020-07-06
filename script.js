1c4703edf858c2525a10568220447876ae2c0717
nechekendu <54294315+nechekendu@users.noreply.github.com>
2020-07-06 20:27:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const menuIconEl = document.querySelector('.menu-icon');
+const sidenavEl = document.querySelector('.sidenav');
+const sidenavCloseEl = document.querySelector('.sidenav__close-icon');
+
+// Add and remove provided class names
+function toggleClassName(el, className) {
+  if (el.hasClass(className)) {
+    el.removeClass(className);
+  } else {
+    el.addClass(className);
+  }
+}
+
+// Open the side nav on click
+menuIconEl.on('click', function() {
+  toggleClassName(sidenavEl, 'active');
+});
+
+// Close the side nav on click
+sidenavCloseEl.on('click', function() {
+  toggleClassName(sidenavEl, 'active');
+});
